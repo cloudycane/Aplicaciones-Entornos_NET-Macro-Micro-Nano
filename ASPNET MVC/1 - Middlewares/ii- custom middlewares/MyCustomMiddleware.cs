f566707a479105ab6ac2d060d5ef0a809b553a84
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-09-19 23:28:53 +0200

    custom middleware

---
+++ ASPNET MVC/1 - Middlewares/ii- custom middlewares/MyCustomMiddleware.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Creacion_Del_Middlewares
+{
+    public class MyCustomMiddleware : IMiddleware // Middleware Interface and Class
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("This is my custom middleware.");
+            await next(context);
+            await context.Response.WriteAsync("Another custom middleware.");
+        }
+    }
+}
