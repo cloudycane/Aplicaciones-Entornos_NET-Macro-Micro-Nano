11dae88e47ce679d766923294b64d5d41278a07c
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-09-20 00:29:29 +0200

    conventional middleware (without inheritance from IMiddleware)

---
+++ ASPNET MVC/1 - Middlewares/iv- (without inheritance IMiddleware) custom conventional middleware class/ConventionalMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Creacion_Del_Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ConventionalMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ConventionalMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Query.ContainsKey("firstname") && httpContext.Request.Query.ContainsKey("LastName"))
+            {
+               string fullname = httpContext.Request.Query["firstname"] + " " + httpContext.Request.Query["lastname"];
+               await httpContext.Response.WriteAsync(fullname);
+            }
+
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ConventionalMiddleware>();
+        }
+    }
+}
---
+++ ASPNET MVC/1 - Middlewares/iv- (without inheritance IMiddleware) custom conventional middleware class/Program.cs
@@ -0,0 +1,41 @@
+using Creacion_Del_Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<IMiddleware, MyCustomMiddleware>();
+
+// ADD THE CONVENTIONAL MIDDLEWARE SERVICE 
+builder.Services.AddTransient<ConventionalMiddleware>();
+
+var app = builder.Build();
+
+// Veamos cómo funcionan los middlewares: continuos and chained.
+// MIDDLEWARE CHAINS- using app.Use for the first middlewares and app.Run() for the last. 
+
+// CUSTOM MIDDLEWARE 
+
+app.UseMiddleware<ConventionalMiddleware>(); // CONVENTIONAL MIDDLEWARE
+
+// MIDDLEWARE 2 or REQUEST DELEGATE 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    var a = "Bienvenidos a mi pagina";
+    var b = "\nMi nombre es Jay";
+    var enunciado = a + b;
+    await context.Response.WriteAsync($"{enunciado}");
+}); 
+
+// MIDDLEWARE 3 or REQUEST DELEGATE 3
+app.Run();
+
+/*
+ 
+ THE PURPOSE OF MIDDLEWARES:  Handle every single methods (only occurs in Program.cs or a customized and heavily typed class)
+ 
+ */
+
+/// <summary>
+/// For example, we create a middleware to handle http redirection, import services, authorization, handling controllers and razor pages and so much more
+/// </summary>
