efb7961e82ce5dcf6b6779a6a28e79477f172b9b
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-09-20 00:06:10 +0200

    middleware extensions

---
+++ ASPNET MVC/1 - Middlewares/iii- custom middlewares extensions (static method)/MyCustomMiddleware.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Creacion_Del_Middlewares
+{
+    public class MyCustomMiddleware : IMiddleware // Middleware Interface and Class
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("This is my custom middleware.");
+            await next(context);
+            await context.Response.WriteAsync("Another custom middleware.");
+        }
+    }
+
+    public static class CustomMiddlewareExtensiom
+    {
+        public static IApplicationBuilder DoSomething(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<MyCustomMiddleware>();
+            
+        }
+    }
+
+
+}
